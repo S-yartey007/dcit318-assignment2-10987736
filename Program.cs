eb02428ee66d682a687f5e1a21ea8ecfc953f16e
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-06-16 09:54:08 +0000

    Inheritance and Method Overriding

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP {
+
+    class Animal {
+        public virtual void MakeSound() {
+            Console.WriteLine("GRRRRRR");
+        }
+    }
+
+    class Dog : Animal {
+        public override void MakeSound()
+        {
+           Console.WriteLine("Bark");
+        }
+    }
+    class Cat : Animal {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program {
+             public static void Main(String[] args) {
+                Animal animal = new Animal();
+                Dog dog = new Dog();
+                Cat cat = new Cat();
+                animal.MakeSound();
+                dog.MakeSound();
+                cat.MakeSound();
+
+        }
+    }
+}
